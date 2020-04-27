dc8dc86cf0e0d68c3892d2a95dce91a3de3f9eeb
Senthil kumar K <nightrider.cbe@gmail.com>
2020-04-27 23:39:20 +0530

    commit

---
+++ SqliteDemo/SqliteDemo/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  SqliteDemo
+//
+//  Created by Senthil on 27/04/20.
+//  Copyright © 2020 Senthil. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SqliteDemo/SqliteDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SqliteDemo
+//
+//  Created by Senthil on 27/04/20.
+//  Copyright © 2020 Senthil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
